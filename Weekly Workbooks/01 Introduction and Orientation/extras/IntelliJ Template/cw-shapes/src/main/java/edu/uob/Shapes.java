0bab2e992d0119249a6b8861c2a2e2d78bd37910
沈荔 <shenhua@eduroam-232-203.nomadic.bris.ac.uk>
2026-01-26 11:18:46 +0000

    Week2 WIP

--- Weekly Workbooks/01 Introduction and Orientation/extras/IntelliJ Template/cw-shapes/src/main/java/edu/uob/Shapes.java
+++ Weekly Workbooks/01 Introduction and Orientation/extras/IntelliJ Template/cw-shapes/src/main/java/edu/uob/Shapes.java
@@ -5,5 +5,19 @@ public class Shapes {
   // TODO use this class as then entry point; play around with your shapes, etc
   public static void main(String[] args) {
     System.out.println("Hello world!");
+    Triangle testTriangle = new Triangle(5, 7, 9);
+    int longestSide = testTriangle.getLongestSide();
+    System.out.println("The longest side of the triangle is " + longestSide);
+
+    //task 3
+    TwoDimensionalShape shape;
+    shape = new Circle(5);
+    System.out.println(shape);//自动调用 Circle.toString()
+
+    shape = new Triangle(5, 6, 7);
+    System.out.println(shape);
+
+    shape = new Rectangle(6,7);
+    System.out.println(shape);
   }
 }
--- Weekly Workbooks/01 Introduction and Orientation/extras/IntelliJ Template/cw-shapes/src/main/java/edu/uob/Triangle.java
+++ Weekly Workbooks/01 Introduction and Orientation/extras/IntelliJ Template/cw-shapes/src/main/java/edu/uob/Triangle.java
@@ -1,9 +1,33 @@
 package edu.uob;
 
-public class Triangle {
-
+public class Triangle extends TwoDimensionalShape{
+  int length1;
+  int length2;
+  int length3;
   // TODO implement me!
-  public Triangle() {}
+  // constructor method
+  public Triangle(int length1, int length2, int length3) {
+    //把参数的值，存进对象自己的成员变量里
+    this.length1 = length1;
+    this.length2 = length2;
+    this.length3 = length3;
+  }
+  public int getLongestSide(){
+    int max;
+    if (length1 > length2){
+      max = length1;
+    }
+    else{
+      max = length2;
+    }
+
+    if (max > length3){
+      return max;
+    }
+    else{
+      return length3;
+    }
+  }
 
   // TODO implement me!
   public double calculateArea() {
@@ -13,5 +37,9 @@ public class Triangle {
   // TODO implement me!
   public int calculatePerimeterLength() {
     return 0;
+  }
+  @override
+  public String toString() {
+    return "Triangle with length1: " + length1 + " Triangle with length2: " + length2 + " Triangle with length3: " + length3;
   }
 }
